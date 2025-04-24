9beebff983620a7c7307e86de1b87855a5e52a7b
Janio Donisete Welnecker <jdwelnecker@sedu.es.gov.br>
2025-04-24 09:10:58 -0300

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,22 @@
 import streamlit as st
 
-st.markdown("## Painel de Apoio à Recomposição das Aprendizagens")
+st.set_page_config(page_title="Painel de Apoio à Recomposição", page_icon="📚")
 
-st.markdown("""
-Olá estudante, seja bem-vindo(a) ao **Painel de Apoio à Recomposição das Aprendizagens**.
+# Insere a imagem de fundo ou capa
+st.image("https://raw.githubusercontent.com/welnecker/questoesama/main/img/fundo.jpg", use_column_width=True)
 
-Nele você terá a oportunidade de testar seu aprendizado e seus conhecimentos atuais em **Matemática** e **Língua Portuguesa**.
+st.markdown("<h3 style='text-align: center;'>Painel de Apoio à Recomposição das Aprendizagens</h3>", unsafe_allow_html=True)
+
+st.write("""
+Olá estudante, seja bem-vindo(a) ao Painel de Apoio à Recomposição das Aprendizagens.
+
+Nele você terá a oportunidade de testar seu aprendizado e seus conhecimentos atuais em Matemática e Língua Portuguesa.
 
 **Não perca esta oportunidade, responda com atenção e só envie as respostas quando tiver certeza, pois só há uma chance de envio.**
 
 **Bons estudos.**
 """)
 
-# Frase final com estilo especial
-st.markdown("""
-<p style='font-family: Georgia, serif; font-size: 18px; color: #444; margin-top: 30px;'>
-Acesse a página <strong>Estudante</strong>, no menu lateral, e faça a atividade proposta pelo seu professor.
-</p>
-""", unsafe_allow_html=True)
+st.markdown("<p style='font-family:monospace; font-size:16px;'>Acesse a página <u><b>Estudante</b></u>, no menu lateral, e faça a atividade proposta pelo seu professor.</p>", unsafe_allow_html=True)
 
 

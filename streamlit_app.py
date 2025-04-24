f10dc216bcc558443110ec8a3446cd477ea6949d
Janio Donisete Welnecker <jdwelnecker@sedu.es.gov.br>
2025-04-24 07:46:23 -0300

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,13 @@
 import streamlit as st
 
-st.set_page_config(page_title="AMA Estudante", page_icon="📘")
-st.markdown("Carregando... use o menu lateral à esquerda para começar. 👉")
+st.set_page_config(page_title="Painel de Apoio à Recomposição das Aprendizagens", page_icon="🎓")
+
+st.title("🎓 Painel de Apoio à Recomposição das Aprendizagens")
+
+st.markdown("""
+### Acesse a página **Estudante** e faça a atividade proposta pelo seu professor.
+
+Responda com atenção e só envie as respostas quando tiver certeza, pois só há uma oportunidade de envio.
+
+**Bons estudos.**
+""")

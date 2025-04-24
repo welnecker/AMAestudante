aae5758ec445b0bab962de742c09b724fd995a84
Janio Donisete Welnecker <jdwelnecker@sedu.es.gov.br>
2025-04-24 08:18:30 -0300

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,32 +3,9 @@ import streamlit as st
 # Configuração da página
 st.set_page_config(page_title="Painel AMA 2025", page_icon="🎓")
 
-# Título centralizado com fonte maior
-st.markdown("""
-<h1 style='text-align: center; font-size: 32px;'>
-    Painel de Apoio à Recomposição das Aprendizagens
-</h1>
-""", unsafe_allow_html=True)
-
-# Espaçamento
-st.markdown("<div style='height: 20px;'></div>", unsafe_allow_html=True)
-
-# Texto de orientação com ênfase na palavra "Estudante"
-st.markdown("""
-<h4 style='font-size: 20px;'>
-    Acesse a página <b><u>Estudante</u></b>, no menu lateral, e faça a atividade proposta pelo seu professor.
-</h4>
-""", unsafe_allow_html=True)
-
-# Espaçamento
-st.markdown("<div style='height: 30px;'></div>", unsafe_allow_html=True)
-
-# Texto explicativo dividido e com fonte maior
 st.markdown("## Painel de Apoio à Recomposição das Aprendizagens")
 
 st.markdown("""
-**Acesse a página _Estudante_, no menu lateral, e faça a atividade proposta pelo seu professor.**
-
 Olá estudante, seja bem-vindo(a) ao **Painel de Apoio à Recomposição das Aprendizagens**.
 
 Nele você terá a oportunidade de testar seu aprendizado e seus conhecimentos atuais em **Matemática** e **Língua Portuguesa**.
@@ -38,5 +15,12 @@ Responda com atenção e **só envie as respostas quando tiver certeza**, pois *
 
 **Bons estudos.**
 """)
+
+# Frase final com fonte diferente
+st.markdown("""
+<p style='font-family: Georgia, serif; font-size: 18px; color: #444; margin-top: 30px;'>
+Acesse a página <strong>Estudante</strong>, no menu lateral, e faça a atividade proposta pelo seu professor.
+</p>
+""", unsafe_allow_html=True)
 
 

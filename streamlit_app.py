767f5f605b4f658a47ce0b7df5aa52af7b9021aa
Janio Donisete Welnecker <jdwelnecker@sedu.es.gov.br>
2025-05-12 08:26:13 -0300

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,3 +40,21 @@ st.markdown("""
     }
     </style>
 """, unsafe_allow_html=True)
+
+
+# Conteúdo com caixa semi-transparente
+st.markdown("<h3 style='text-align: center;'>Painel de Apoio à Recomposição das Aprendizagens</h3>", unsafe_allow_html=True)
+
+st.markdown("""
+<div class="custom-text">
+<p>Olá estudante, seja bem-vindo(a) ao <strong>Painel de Apoio à Recomposição das Aprendizagens</strong>.</p>
+
+<p>Nele você terá a oportunidade de testar seu aprendizado e seus conhecimentos atuais em <strong>Matemática</strong> e <strong>Língua Portuguesa</strong>.</p>
+
+<p><strong>Não perca esta oportunidade</strong>, responda com atenção e <strong>só envie as respostas quando tiver certeza</strong>, pois <strong>só há uma chance de envio</strong>.</p>
+
+<p><strong>Bons estudos.</strong></p>
+
+<p style='font-family:monospace; font-size:16px;'>Acesse a página <u><b>Estudante</b></u>, no menu lateral, e faça a atividade proposta pelo seu professor.</p>
+</div>
+""", unsafe_allow_html=True)
